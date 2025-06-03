9806d78278e117633d41022226ee497d460f9b04
AlexDals <100142352+AlexDals@users.noreply.github.com>
2025-06-03 10:10:56 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -500,13 +500,11 @@ with st.container():
     st.markdown(f'<div class="current-temp">{cct_result:.0f}K</div>', unsafe_allow_html=True)
     st.markdown(f'<div class="temp-description">{color_description}</div>', unsafe_allow_html=True)
     
-    col1, col2 = st.columns([1, 1])
+    col1 = st.columns([1])
     
     with col1:
         # Color information
-    
-    with col2:
-        # Color swatch
+ # Color swatch
         text_color = 'black' if sum([r, g, b]) > 400 else 'white'
         st.markdown(
             f'''
@@ -520,6 +518,7 @@ with st.container():
             unsafe_allow_html=True
         )
 
+       
 # Reference Colors
 with st.container():
     st.markdown('<div class="section-header">🌡️ Reference Temperatures</div>', unsafe_allow_html=True)
